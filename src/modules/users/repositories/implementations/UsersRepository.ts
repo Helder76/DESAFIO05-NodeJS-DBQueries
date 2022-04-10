c31054fe494fae8435cd8e18eba396ed23075a0f
Helder Freitas <freitas.h@gmail.com>
2022-04-10 13:05:22 -0300

    final commit

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,40 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await
+      this
+        .repository
+        .findOneOrFail({
+          relations: ['games'],
+          where: { id: user_id }
+        });
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    return this
+      .repository
+      .query(`
+        SELECT    US.* 
+        FROM      users US 
+        ORDER BY  US.first_name
+      `);
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    return this
+      .repository
+      .query(`
+          SELECT  US.* 
+          FROM    users US 
+          WHERE   US.first_name ILIKE $1 
+                  AND US.last_name ILIKE $2
+        `
+        , [first_name, last_name]);
   }
 }
